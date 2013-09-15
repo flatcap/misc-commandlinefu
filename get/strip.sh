bdd8f62e0feb265330d792e061d9166776c6425c
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-09-15 19:17:22 +0100

    strip

--- get/strip.sh
+++ get/strip.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#xmlstarlet edit					\
-#	--delete '//_:div[@id="cloud"]'		\
-#	--delete '//_:head'			\
-#	--delete '//_:script'			\
-#	--delete '//_:dl[@id="news"]'		\
-#	--delete '//_:img[@class="gravatar"]'	\
-#	x2.html | tidy -q | unexpand > x3.html
-
 xmlstarlet edit						\
 	--delete '//_:script'				\
 	--delete '//_:head'				\
@@ -22,12 +14,5 @@ xmlstarlet edit						\
 	--delete '//_:div[@class="meta"]'		\
 	--delete '//_:div[@class="votes"]'		\
 	--delete '//_:div[@class="options"]'		\
-	x1.html > x2.html
-
-exit
-	x1.html > x2.html
-
-#xmlstarlet select -t					\
-#	-c '//_:div[@id="left-block"]'			\
-#	x2.html > x3.html
+	$1 | sponge $1
 
