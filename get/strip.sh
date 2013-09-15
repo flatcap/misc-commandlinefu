f05b84c2e51b4320f4ed991ab84c335234219384
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-09-15 19:30:16 +0100

    tidy

--- get/strip.sh
+++ get/strip.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+#xmlstarlet edit						\
+#	--delete '//_:script'				\
+#	--delete '//_:head'				\
+#	--delete '//_:img[@class="gravatar"]'		\
+#	--delete '//_:div[@id="right-block"]'		\
+#	--delete '//_:div[@id="flash"]'			\
+#	--delete '//_:div[@id="header"]'		\
+#	--delete '//_:div[@class="affiliates"]'		\
+#	--delete '//_:div[@class="advertising"]'	\
+#	--delete '//_:div[@class="window-bar"]'		\
+#	--delete '//_:div[@class="output-meta"]'	\
+#	--delete '//_:div[@class="meta"]'		\
+#	--delete '//_:div[@class="votes"]'		\
+#	--delete '//_:div[@class="options"]'		\
+#	$1 | sponge $1
+
 xmlstarlet edit						\
-	--delete '//_:script'				\
-	--delete '//_:head'				\
 	--delete '//_:img[@class="gravatar"]'		\
 	--delete '//_:div[@id="right-block"]'		\
 	--delete '//_:div[@id="flash"]'			\
@@ -15,4 +29,5 @@ xmlstarlet edit						\
 	--delete '//_:div[@class="votes"]'		\
 	--delete '//_:div[@class="options"]'		\
 	$1 | sponge $1
+
 
